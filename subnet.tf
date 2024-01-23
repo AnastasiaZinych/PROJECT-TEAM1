1e6cfaed87a008b927609d49975527bfa787270c
AnastasiiaZinych <zinychnastya@gmail.com>
2024-01-23 21:20:49 +0000

    add vpc and subnet

---
+++ subnet.tf
@@ -0,0 +1,45 @@
+resource "aws_subnet" "public1" {
+  vpc_id = aws_vpc.Project.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1"
+  tags = team1
+}
+ 
+resource "aws_subnet" "public2" {
+  vpc_id = aws_vpc.Project.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1"
+  tags = team1
+}
+ 
+resource "aws_subnet" "public3" {
+  vpc_id = aws_vpc.Project.id
+  cidr_block = "10.0.3.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1"
+  tags = team1
+}
+
+resource "aws_subnet" "private1" {
+  vpc_id = aws_vpc.Project.id
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "us-east-1"
+  tags = team1
+}
+ 
+resource "aws_subnet" "private2" {
+  vpc_id = aws_vpc.Project.id
+  cidr_block = "10.0.5.0/24"
+  availability_zone = "us-east-1"
+  tags = team1
+}
+ 
+resource "aws_subnet" "private3" {
+  vpc_id = aws_vpc.Project.id
+  cidr_block = "10.0.6.0/24"
+  availability_zone = "us-east-1"
+  tags = team1
+}
+ 
